035f94b1fa8f9074e503a1ef431c9c9ff4b2bb72
Dandingo <BQ72053@umbc.edu>
2025-02-21 17:35:15 -0500

    test(auth): Added unit tests to cover the func GetAPIKey to 100% coverage

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,40 @@
+package auth
+
+import (
+	"reflect"
+	"testing"
+)
+
+// Happy Path
+func TestGetAPIKey(t * testing.T) {
+	// Arrange
+	testCases := []map[string][]string {
+		{
+			"Authorization": {"ApiKey some_token"},
+		},
+		{
+			"Authorization": {""},
+		},
+		{
+			"Authorization": {"Bearer some_token_bearer"},
+		},	
+	}
+
+	// Act
+	got, err := GetAPIKey(testCases[0])
+	want := "some_token"
+	// Assert
+	if (!reflect.DeepEqual(want, got)) {
+		t.Fatalf("expected: %v, got: %v, err: %v", want, got, err)
+	}
+
+	for _, tc := range testCases[1:] {
+		got, err := GetAPIKey(tc)
+		want := ""
+		// Assert
+		if (!reflect.DeepEqual(want, got)) {
+			t.Errorf("expected: %v, got: %v, err: %v", want, got, err)
+		}
+	}
+	
+}
